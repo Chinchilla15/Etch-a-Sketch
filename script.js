b7e1bd9d33a4bf9111980e749212d8e01f4ab161
Alessandro Chinchilla <ale030401@gmail.com>
2023-06-26 16:15:10 -0600

    divs change color working

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const input = document.querySelector("#rangeInput");
-const container = document.querySelector("#container");
+const input = document.getElementById("rangeInput");
+const container = document.getElementById("container");
 const rowSize = document.querySelector(".rangeValue2");
 const colSize = document.querySelector(".rangeValue");
-
+const color = document.getElementById('color');
+const rainbow = document.getElementById('rainbow');
+const grayScale = document.getElementById('grayScale');
+const eraser = document.getElementById('eraser');
+const clear = document.getElementById('clear');
 
 // Update rowSize and colSize values on input change
 input.addEventListener('input', (e) => {
@@ -27,3 +31,20 @@ function createGrid(size) {
 }
 
 createGrid(parseInt(input.value));
+
+container.addEventListener("mouseover", (e) => {
+    e.target.style.background = "black";
+})
+
+
+/*
+clear.addEventListener('click', ()=>{
+    container.style.background = "white"
+})
+*/
+
+function white(){
+    container.addEventListener("mouseover", (e) => {
+        e.target.style.background = "white";
+    })
+}
