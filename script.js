62a87757cfedba31977243edb5ddb30effb66b4f
Alessandro Chinchilla <ale030401@gmail.com>
2023-06-26 18:01:37 -0600

    Hover color change done, rainbow and eraser button done

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const rowSize = document.querySelector(".rangeValue2");
 const colSize = document.querySelector(".rangeValue");
 const color = document.getElementById('color');
 const rainbow = document.getElementById('rainbow');
-const grayScale = document.getElementById('grayScale');
+const gray = document.getElementById('gray');
 const eraser = document.getElementById('eraser');
 const clear = document.getElementById('clear');
+const divs = container.getElementsByTagName("div");
 
 // Update rowSize and colSize values on input change
 input.addEventListener('input', (e) => {
@@ -32,19 +33,44 @@ function createGrid(size) {
 
 createGrid(parseInt(input.value));
 
-container.addEventListener("mouseover", (e) => {
-    e.target.style.background = "black";
-})
+function blackColor(){
+    container.addEventListener("mouseover", (e) => {
+        e.target.style.background = "black";
+    })
+}
 
+function rainbowColor(){
+    container.addEventListener("mouseover", (e) => {
+        e.target.style.background = "#" + (Math.random()*0xFFFFFF<<0).toString(16);
+    })
+}
 
-/*
-clear.addEventListener('click', ()=>{
-    container.style.background = "white"
-})
-*/
-
-function white(){
+function eraserButton(){
     container.addEventListener("mouseover", (e) => {
         e.target.style.background = "white";
     })
 }
+
+function clearContainer(){
+    
+}
+
+color.addEventListener("click",()=>{
+    blackColor();
+})
+
+rainbow.addEventListener("mouseover", ()=>{
+    rainbowColor();
+})
+
+gray.addEventListener("click",()=>{
+    alert("Gray Scale Clicked!")
+})
+
+eraser.addEventListener('click', () => {
+    eraserButton();
+})
+
+clear.addEventListener('click', ()=>{
+    clearContainer();
+})
